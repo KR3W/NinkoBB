509f2c6705e370500ea079068853f7c78d086334
Nijikokun <me@nijikokun.com>
2010-02-06 03:41:59 -0500

    First Backup

---
+++ include/database.php
@@ -0,0 +1,15 @@
+<?php if(!defined('IN_NK')) die('Invalid inclusion.');
+/**
+ * database.php
+ * 
+ * Controls the database connection values
+ * @version 1.1
+ * @copyright (c) 2010 ANIGAIKU
+ * @package ninko
+ */
+
+$config['db'] = 'ninkobb'; // Database
+$config['user'] = 'root'; // MySQL Username 
+$config['pass'] = 'kilgore'; // MySQL Password 
+$config['host'] = 'localhost'; // MySQL Host
+?>
